315035755b2cd4c96dc349a217ff27d1a0fa8baa
Rodger Maganha <maganharodger@gmail.com>
2025-03-31 23:16:34 -0300

    Update forno.py

--- forno.py
+++ forno.py
@@ -65,7 +65,7 @@ st.subheader("📈 Evolução da Temperatura")
 grafico_display = st.empty()
 
 # Loop para gerar e atualizar os dados
-while true:  # Pode ser ajustado
+while True:  # Pode ser ajustado
     novo_dado = gerar_dado()
 
     # Adiciona ao histórico
